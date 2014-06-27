c1ce9ed39619eb2936d83c2507a4fdc2f554280a
mainguyen89mt07@gmail.com <mainguyen89mt07@gmail.com@4cf296cd-f3ea-2c35-570d-3220d5f9d0de>
2014-06-27 10:29:24 +0000

    import address

--- trunk/include/class_import.php
+++ trunk/include/class_import.php
@@ -19,15 +19,15 @@ class HOMEImport {
         foreach ($arr as $value) {
             try {
                 $roomStatus = 0;
-                $tmp = mb_convert_encoding((string)@$value[7], "UTF-8", "Shift-JIS");
-                if($tmp == mb_convert_encoding('居住中', "UTF-8", "Shift-JIS")){
+                $tmp = mb_convert_encoding((string) @$value[7], "UTF-8", "Shift-JIS");
+                if ($tmp == mb_convert_encoding('居住中', "UTF-8", "Shift-JIS")) {
                     $roomStatus = 1;
-                }elseif($tmp == mb_convert_encoding('未完成', "UTF-8", "Shift-JIS")){
+                } elseif ($tmp == mb_convert_encoding('未完成', "UTF-8", "Shift-JIS")) {
                     $roomStatus = 2;
                 }
                 $broker_id = $this->_getBrokerId(array('name' => (string) @$value[12], 'undertake' => (string) @$value[13], 'phone' => (string) @$value[14]));
-                $house_id = $this->_getHouseId(array('name' => (string) @$value[5], 'address' => (string) @$value[4], 'type' => (string) @$houseType[mb_convert_encoding((string)@$value[3], "UTF-8", "Shift-JIS")]));
-                $room->create_room((string) @$value[6], '', '', (int)$roomStatus, (string) @$value[8], (string) @$value[10], (string) @$value[9], (string) @$value[11], '', '', $house_id, $broker_id);
+                $house_id = $this->_getHouseId(array('name' => (string) @$value[5], 'address' => (string) @$value[4], 'type' => (string) @$houseType[mb_convert_encoding((string) @$value[3], "UTF-8", "Shift-JIS")]));
+                $room->create_room((string) @$value[6], '', '', (int) $roomStatus, (string) @$value[8], (string) @$value[10], (string) @$value[9], (string) @$value[11], '', '', $house_id, $broker_id);
             } catch (Exception $ex) {
                 continue;
             }
@@ -82,5 +82,178 @@ class HOMEImport {
         }
         return $arr;
     }
-    
+
+    public function importAddress($arr = array()) {
+        if (empty($arr)) {
+            return;
+        }
+        foreach ($arr as $key => $value) {
+            try {
+                $street_name = '';
+                $ward_name = '';
+                if(count($value) == 4){
+                    $value[2] = $value[3];
+                    $value[1] = $value[2];
+                    $value[0] = $value[1];
+                    unset($value[3]);
+                }
+                if (@strpos($value[1], ",") !== false) {
+                    $value[2] = $value[1];
+                    $value[1] = $value[0];
+                    $value[0] = @$arr[$key - 1][2];
+                }
+                if (@strpos($value[2], ",") !== false) {
+                    $tmp = explode(",", $value[2]);
+                    if(count($tmp)){
+                        $street_name = $tmp[0];
+                        $ward_name = $tmp[1];
+                    }
+                }
+                $city = $this->_create_city($value[0]);
+                $district = $this->_create_district($city, $value[1]);
+                if(!empty($street_name)){
+                    $street = $this->_create_street($district, $street_name);
+                }
+                if(!empty($ward_name)){
+                    $this->_create_ward($street, $ward_name);
+                }
+            } catch (Exception $e) {
+                echo $key . implode('===', $value);
+                continue;
+            }
+        }
+    }
+
+    private function _create_city($city_name) {
+
+        global $database;
+
+        $city_name = trim($city_name);
+
+        $id = $this->_checkExistCity($city_name);
+        if (!empty($id)) {
+            return $id;
+        }
+
+        $query = "INSERT INTO house_city(`city_name`) VALUES ('{$city_name}')";
+        $result = $database->database_query($query);
+        if ($result) {
+            return (int) $database->database_insert_id();
+        } else {
+            return false;
+        }
+    }
+
+    private function _checkExistCity($city_name) {
+
+        global $database;
+
+        $query = "SELECT * FROM house_city WHERE city_name='{$city_name}'";
+        $result = $database->database_query($query);
+        $row = $database->database_fetch_assoc($result);
+        if (!empty($row)) {
+            return (int) $row['id'];
+        }
+        return false;
+    }
+
+    private function _create_district($city_id, $district_name) {
+
+        global $database;
+
+        $district_name = trim($district_name);
+
+        $id = $this->_checkExistDistrict($city_id, $district_name);
+        if (!empty($id)) {
+            return $id;
+        }
+        $query = "INSERT INTO house_district(`district_name`,`city_id`) VALUES('{$district_name}','{$city_id}')";
+
+        $result = $database->database_query($query);
+        if ($result) {
+            return (int) $database->database_insert_id();
+        } else {
+            return false;
+        }
+    }
+
+    private function _checkExistDistrict($city_id, $district_name) {
+
+        global $database;
+
+        $query = "SELECT * FROM house_district WHERE district_name='{$district_name}' AND city_id='{$city_id}'";
+        $result = $database->database_query($query);
+        $row = $database->database_fetch_assoc($result);
+        if (!empty($row)) {
+            return (int) $row['id'];
+        }
+        return false;
+    }
+
+    private function _create_street($district_id, $street_name) {
+
+        global $database;
+
+        $street_name = trim($street_name);
+
+        $id = $this->_checkExistStreet($district_id, $street_name);
+        if (!empty($id)) {
+            return $id;
+        }
+
+        $query = "INSERT INTO house_street(`street_name`,`district_id`) VALUES('{$street_name}','{$district_id}')";
+        $result = $database->database_query($query);
+        if ($result) {
+            return (int) $database->database_insert_id();
+        } else {
+            return false;
+        }
+    }
+
+    private function _checkExistStreet($district_id, $street_name) {
+
+        global $database;
+
+        $query = "SELECT * FROM house_street WHERE street_name='{$street_name}' AND district_id='{$district_id}'";
+        $result = $database->database_query($query);
+        $row = $database->database_fetch_assoc($result);
+        if (!empty($row)) {
+            return (int) $row['id'];
+        }
+        return false;
+    }
+
+    private function _create_ward($street_id, $ward_name) {
+
+        global $database;
+
+        $ward_name = trim($ward_name);
+
+        $id = $this->_checkExistWard($street_id, $ward_name);
+        if (!empty($id)) {
+            return $id;
+        }
+
+        $query = "INSERT INTO house_ward(`ward_name`,`street_id`) VALUES('{$ward_name}','{$street_id}')";
+        $result = $database->database_query($query);
+        if ($result) {
+            return (int) $database->database_insert_id();
+        } else {
+            return false;
+        }
+    }
+
+    private function _checkExistWard($street_id, $ward_name) {
+
+        global $database;
+
+        $query = "SELECT * FROM house_ward WHERE ward_name='{$ward_name}' AND street_id='{$street_id}'";
+        $result = $database->database_query($query);
+        $row = $database->database_fetch_assoc($result);
+        if (!empty($row)) {
+            return (int) $row['id'];
+        }
+        return false;
+    }
+
 }
