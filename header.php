51c625ee0df6b26bc6c459f5011fd39b37fe21af
mainguyen89mt07 <mainguyen89mt07@gmail.com>
2014-11-27 06:08:10 +0000

    19/11

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
-date_default_timezone_set("Europe/Berlin");
+date_default_timezone_set('UTC');
 //require('include/config/config.php');
 //
 //$smarty=new smarty_class();
