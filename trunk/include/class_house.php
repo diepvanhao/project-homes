0a75fd8f20974e3e9f7eb29469a9fe5d2795cd99
mainguyen89mt07@gmail.com <mainguyen89mt07@gmail.com@4cf296cd-f3ea-2c35-570d-3220d5f9d0de>
2015-03-27 10:32:56 +0000

    26-03-2015

--- trunk/include/class_house.php
+++ trunk/include/class_house.php
@@ -511,11 +511,8 @@ class HOMEHouse {
     }
 
     function getGroup($search = "", $offset = 0, $length = 50) {
-        global $database,$user; 
-        $agent_id = $user->user_info['agent_id'];
-        $level = $user->user_info['user_authorities'];
-        
-        $query = "select * from home_group AS g LEFT JOIN home_user AS u ON g.user_id = u.id WHERE ( u.agent_id = {$agent_id} OR {$level} = 1)";
+        global $database;
+        $query = "select * from home_group";
         if (!empty($search))
             $query.=" where group_name like '%{$search}%'";
         $query.=" ORDER BY group_name ASC ";
